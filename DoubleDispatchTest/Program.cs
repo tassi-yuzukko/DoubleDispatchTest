d6082d781a2e48c7888eb374cb79b89eff37ef6f
Kei Tashima <tassi.yuzukko@gmail.com>
2017-10-29 10:47:51 +0900

    プロジェクト ファイルを追加します。

---
+++ DoubleDispatchTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoubleDispatchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
